d89c3350630e802d7144239f0eee4f561811ea80
Dariusz <dariusz.laciak@gmail.com>
2016-02-10 23:03:50 +0100

    Zadanie 7 poprawione

--- src/Laciak_07.java
+++ src/Laciak_07.java
@@ -8,20 +8,6 @@ import java.util.function.BooleanSupplier;
 class AnyAlgorithm implements AnyAlgorithmInterface {
     private ExecutionListInterface list;
 
-
-    public ExecutionList createList(){
-        return new ExecutionList();
-    }
-
-    public Fork createFork(){
-        return new Fork();
-    }
-
-
-    public Loop createLoop(){
-        return new Loop();
-    }
-
     @Override
     public void setList(ExecutionListInterface list) {
         this.list = list;
@@ -33,7 +19,7 @@ class AnyAlgorithm implements AnyAlgorithmInterface {
             list.start();
     }
 
-    public class ExecutionList implements ExecutionListInterface{
+    public static class ExecutionList implements ExecutionListInterface {
         private List<Object> objects;
 
         public ExecutionList(){
@@ -62,21 +48,17 @@ class AnyAlgorithm implements AnyAlgorithmInterface {
 
         @Override
         public void start() {
-            for(Object o : objects){
-                if(o != null){
-                    if(o instanceof Runnable){
-                        ((Runnable) o).run();
-                    }
-                    else {
-                        ((ExecutableInterface) o).start();
-                    }
+            objects.stream().filter(o -> o != null).forEach(o -> {
+                if (o instanceof Runnable) {
+                    ((Runnable) o).run();
+                } else {
+                    ((ExecutableInterface) o).start();
                 }
-
-            }
+            });
         }
     }
 
-    public class Loop implements LoopInterface{
+    public static class Loop implements LoopInterface {
         private ExecutionListInterface list;
         private BooleanSupplier continuationCondition;
         private boolean preFlag;
@@ -113,7 +95,7 @@ class AnyAlgorithm implements AnyAlgorithmInterface {
         }
     }
 
-    public class Fork implements ForkInterface{
+    public static class Fork implements ForkInterface {
         private BooleanSupplier forkCondition;
         private ExecutionListInterface trueList;
         private ExecutionListInterface falseList;
@@ -146,105 +128,4 @@ class AnyAlgorithm implements AnyAlgorithmInterface {
             }
         }
     }
-}
-
-interface AnyAlgorithmInterface extends ExecutableInterface {
-
-    /**
-     * Interfejs budowy listy polecen do wykonania. W sklad
-     * listy moga wchodzic inne listy, petle, rozgalezienia i
-     * obiekty Runnable. Nazwa ExecutionList zostala wprowadzona
-     * w celu unikniecia problemow z interfejsem List.
-     */
-    interface ExecutionListInterface extends ExecutableInterface {
-        /**
-         * Dodanie jednej operacji do wykonania.
-         * @param run obiekt zgodny z interfejsem Runnable
-         */
-        void add( Runnable run );
-
-        /**
-         * Dodaje liste operacji do wykonania.
-         * @param list lista operacji do wykonania
-         */
-        void add( ExecutionListInterface list );
-
-        /**
-         * Dodaje petle.
-         * @param loop petla do wykonania
-         */
-        void add( LoopInterface loop );
-
-        /**
-         * Dodaje operacje warunkowa.
-         * @param fork operacja do wykonania
-         */
-        void add( ForkInterface fork );
-    }
-
-    /**
-     * Interfejs budowy petli. Petla sklada sie z listy
-     * operacji do wykonania i warunku kontynuacji.
-     * Warunek sprawdzany jest przed lub po kazdorazowym wykonaniu
-     * kompletu operacji nalezacych do listy.
-     */
-    interface LoopInterface extends ExecutableInterface {
-        /**
-         * Ustawia liste operacji do wykonania w petli.
-         * @param list lista operacji do wykonania
-         */
-        void set( ExecutionListInterface list );
-
-        /**
-         * Ustawia warunek kontynuacji.
-         * @param continuationCondition obiekt zgodny z interfejsem funkcyjnym
-         * BooleanSupplier. Prawda logiczna oznacza, ze dzialanie petli ma byc
-         * kontynuowane.
-         * @param preFlag flaga okreslajaca czy warunek ma byc sprawdzany
-         * przed wykonaniem listy operacji (true) czy po jej wykonaniu (false).
-         */
-        void set( BooleanSupplier continuationCondition, boolean preFlag );
-    }
-
-    /**
-     * Interfejs budowy rozgalezienia. Elementami
-     * skladowymi sa warunek wyboru sciezki wykonania oraz
-     * listy operacji do wykonania w przypadku
-     * wyboru danej sciezki. Warunek sprawdzany jest jako
-     * pierwszy - od uzyskanego wyniku zalezy, ktora
-     * z dwoch sciezek zostanie wybrana.
-     */
-    interface ForkInterface extends ExecutableInterface {
-        /**
-         * Ustawia warunek, ktory zostanie uzyty do podjecia decyzji,
-         * ktora z galezi bedzie realizowana.
-         * @param forkCondition warunek
-         */
-        void set( BooleanSupplier forkCondition );
-        /**
-         * Lista operacji do realizacji jesli warunek okaze sie prawda.
-         * @param list lista operacji do wykonania dla prawdy
-         */
-        void setTrueBranch( ExecutionListInterface list );
-
-        /**
-         * Lista operacji do realizacji jesli warunek okaze sie falszem.
-         * @param list lista operacji do wykonania w przypadku falszu
-         */
-        void setFalseBranch( ExecutionListInterface list );
-    }
-
-    /**
-     * Ustawia liste polecen do wykonania.
-     *
-     * @param list - lista polecen do wykonania
-     */
-    void setList( ExecutionListInterface list );
-}
-
-interface ExecutableInterface {
-    /**
-     * Metoda zleca rozpoczecie wykonania.
-     */
-    void start();
 }
